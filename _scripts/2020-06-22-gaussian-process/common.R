c9c273889ef0518edf0c8a68e87494083d0a6d2c
Ivan Ukhov <ivan.ukhov@gmail.com>
2020-06-19 17:33:57 +0200

    Justify the choice of priors for the noise

--- _scripts/2020-06-22-gaussian-process/common.R
+++ _scripts/2020-06-22-gaussian-process/common.R
@@ -11,17 +11,17 @@ distance <- function(x, x_prime) {
   sqrt(pmax(x_2 + x_prime_2 - 2 * x_x_prime, 0))
 }
 
-prior <- function(x, jitter = 1e-6, ...) {
+process_prior <- function(x, jitter = 1e-6, ...) {
   m <- nrow(x);
   U <- chol(covariance(x, x, ...) + diag(jitter, m));
   as.vector(rnorm(m) %*% U)
 }
 
-prior_map <- function(x, ...) {
-  tibble(x = x, y = prior(as.matrix(x), ...))
+process_prior_map <- function(x, ...) {
+  tibble(x = x, y = process_prior(as.matrix(x), ...))
 }
 
-prior_plot <- function(n = 10, x = seq(0, 1, by = 0.01),
+process_prior_plot <- function(n = 10, x = seq(0, 1, by = 0.01),
                        sigma_process = 1, ell_process = 1) {
   tibble(.draw = factor(seq(1, n)),
          x = list(x),
@@ -31,10 +31,37 @@ prior_plot <- function(n = 10, x = seq(0, 1, by = 0.01),
               curve = pmap(list(x = x,
                                 sigma_process = sigma_process,
                                 ell_process = ell_process),
-                           prior_map)) %>%
+                           process_prior_map)) %>%
     unnest(curve) %>%
-    ggplot(aes(x, y, color = .draw)) +
-    geom_line(size = 1) +
+    ggplot(aes(x, y, group = .draw)) +
+    geom_line(size = 0.75) +
     labs(x = 'Distance', y = 'Response') +
+    theme(legend.position = 'none')
+}
+
+noise_prior <- function(x, alpha_noise, beta_noise) {
+  m <- nrow(x);
+  sigma_noise <- as.vector(exp(alpha_noise + x %*% beta_noise));
+  sigma_noise * rnorm(m)
+}
+
+noise_prior_map <- function(x, ...) {
+  tibble(x = x, y = noise_prior(as.matrix(x), ...))
+}
+
+noise_prior_plot <- function(n = 10, x = seq(0, 1, by = 0.01), sigma = 1) {
+  tibble(.draw = factor(seq(1, n)),
+         x = list(x),
+         alpha_noise = rnorm(n, sd = sigma),
+         beta_noise = rnorm(n, sd = sigma)) %>%
+    transmute(.draw = .draw,
+              curve = pmap(list(x = x,
+                                alpha_noise = alpha_noise,
+                                beta_noise = beta_noise),
+                           noise_prior_map)) %>%
+    unnest(curve) %>%
+    ggplot(aes(x, y, group = .draw)) +
+    geom_line(size = 0.75, alpha = 0.2) +
+    labs(x = 'Distance', y = 'Noise') +
     theme(legend.position = 'none')
 }
