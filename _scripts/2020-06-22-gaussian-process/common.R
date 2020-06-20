5dafe8136aa798015e2df232263995f96f4a22d0
Ivan Ukhov <ivan.ukhov@gmail.com>
2020-06-20 14:20:27 +0200

    Change the prior for the noise

--- _scripts/2020-06-22-gaussian-process/common.R
+++ _scripts/2020-06-22-gaussian-process/common.R
@@ -11,17 +11,55 @@ distance <- function(x, x_prime) {
   sqrt(pmax(x_2 + x_prime_2 - 2 * x_x_prime, 0))
 }
 
-process_prior <- function(x, jitter = 1e-6, ...) {
+prior_length_scale_plot <- function(x = seq(0.01, 5, by = 0.01),
+                                    alpha = 1, beta = 1) {
+  tibble(x = x) %>%
+    mutate(y = dinvgamma(x, alpha, beta)) %>%
+    ggplot(aes(x, y)) +
+    geom_line() +
+    labs(x = 'Length scale', y = 'Prior density')
+}
+
+prior_noise <- function(x, alpha_noise, beta_noise) {
+  m <- nrow(x);
+  sigma_noise <- sqrt(exp(alpha_noise + x %*% beta_noise));
+  rnorm(m, sd = sigma_noise)
+}
+
+prior_noise_map <- function(x, ...) {
+  tibble(x = x, y = prior_noise(as.matrix(x), ...))
+}
+
+prior_noise_plot <- function(n = 5, x = seq(0, 1, by = 0.01),
+                             alpha = list(mu = 0, sigma = 1, nu = 3),
+                             beta = list(mean = 0, sd = 1)) {
+  tibble(.draw = factor(seq(1, n)),
+         x = list(x),
+         alpha_noise = do.call(rst, c(list(n = n), alpha)),
+         beta_noise = do.call(rnorm, c(list(n = n), beta))) %>%
+    transmute(.draw = .draw,
+              curve = pmap(list(x = x,
+                                alpha_noise = alpha_noise,
+                                beta_noise = beta_noise),
+                           prior_noise_map)) %>%
+    unnest(curve) %>%
+    ggplot(aes(x, y, color = .draw)) +
+    geom_line(size = 0.75, alpha = 0.5) +
+    labs(x = 'Distance', y = 'Noise') +
+    theme(legend.position = 'none')
+}
+
+prior_process <- function(x, jitter = 1e-6, ...) {
   m <- nrow(x);
   U <- chol(covariance(x, x, ...) + diag(jitter, m));
   as.vector(rnorm(m) %*% U)
 }
 
-process_prior_map <- function(x, ...) {
-  tibble(x = x, y = process_prior(as.matrix(x), ...))
+prior_process_map <- function(x, ...) {
+  tibble(x = x, y = prior_process(as.matrix(x), ...))
 }
 
-process_prior_plot <- function(n = 10, x = seq(0, 1, by = 0.01),
+prior_process_plot <- function(n = 10, x = seq(0, 1, by = 0.01),
                        sigma_process = 1, ell_process = 1) {
   tibble(.draw = factor(seq(1, n)),
          x = list(x),
@@ -31,37 +69,10 @@ process_prior_plot <- function(n = 10, x = seq(0, 1, by = 0.01),
               curve = pmap(list(x = x,
                                 sigma_process = sigma_process,
                                 ell_process = ell_process),
-                           process_prior_map)) %>%
+                           prior_process_map)) %>%
     unnest(curve) %>%
     ggplot(aes(x, y, group = .draw)) +
     geom_line(size = 0.75) +
     labs(x = 'Distance', y = 'Response') +
-    theme(legend.position = 'none')
-}
-
-noise_prior <- function(x, alpha_noise, beta_noise) {
-  m <- nrow(x);
-  sigma_noise <- as.vector(exp(alpha_noise + x %*% beta_noise));
-  sigma_noise * rnorm(m)
-}
-
-noise_prior_map <- function(x, ...) {
-  tibble(x = x, y = noise_prior(as.matrix(x), ...))
-}
-
-noise_prior_plot <- function(n = 10, x = seq(0, 1, by = 0.01), sigma = 1) {
-  tibble(.draw = factor(seq(1, n)),
-         x = list(x),
-         alpha_noise = rnorm(n, sd = sigma),
-         beta_noise = rnorm(n, sd = sigma)) %>%
-    transmute(.draw = .draw,
-              curve = pmap(list(x = x,
-                                alpha_noise = alpha_noise,
-                                beta_noise = beta_noise),
-                           noise_prior_map)) %>%
-    unnest(curve) %>%
-    ggplot(aes(x, y, group = .draw)) +
-    geom_line(size = 0.75, alpha = 0.2) +
-    labs(x = 'Distance', y = 'Noise') +
     theme(legend.position = 'none')
 }
